92e4424ba8b2328074902891763d8e1e4c8408fe
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2023-05-28 15:16:38 +0900

    added animation mixamo

--- Assets/Scripts/Model/Unit/UnitMover.cs
+++ Assets/Scripts/Model/Unit/UnitMover.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class UnitMover : MonoBehaviour, IService
 {
     [SerializeField] private float _forwardSpeed;
@@ -9,11 +10,13 @@ public class UnitMover : MonoBehaviour, IService
 
     private Fsm _fsm;
     private Rigidbody _rigidBody;
+    private Animator _animator;
 
     private void Start()
     {
         _fsm = new Fsm();
         _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
 
         _fsm.AddState(new StateIdle(_fsm));
         _fsm.AddState(new StateForward(_fsm));
@@ -37,30 +40,35 @@ public class UnitMover : MonoBehaviour, IService
     public void Stay()
     {
         _rigidBody.velocity = Vector3.zero;
+        _animator.SetTrigger(AnimationParameters.Params.Stay);
     }
 
     public void MoveForward()
     {
-        _rigidBody.velocity += transform.forward * _forwardSpeed * Time.deltaTime;
+        _rigidBody.velocity = transform.forward * _forwardSpeed * Time.deltaTime;
         Clamp(transform.forward, _forwardSpeed);
+        _animator.SetTrigger(AnimationParameters.Params.Walk);
     }
 
     public void MoveBack()
     {
-        _rigidBody.velocity -= transform.forward * _backSpeed * Time.deltaTime;
+        _rigidBody.velocity = -transform.forward * _backSpeed * Time.deltaTime;
         Clamp(-transform.forward, _backSpeed);
+        _animator.SetTrigger(AnimationParameters.Params.Back);
     }
 
     public void MoveLeft()
     {
-        _rigidBody.velocity -= transform.right * _sideSpeed * Time.deltaTime;
+        _rigidBody.velocity = -transform.right * _sideSpeed * Time.deltaTime;
         Clamp(-transform.right, _sideSpeed);
+        _animator.SetTrigger(AnimationParameters.Params.Left);
     }
 
     public void MoveRight()
     {
-        _rigidBody.velocity += transform.right * _sideSpeed * Time.deltaTime;
+        _rigidBody.velocity = transform.right * _sideSpeed * Time.deltaTime;
         Clamp(transform.right, _sideSpeed);
+        _animator.SetTrigger(AnimationParameters.Params.Right);
     }
 
     private void Clamp(Vector3 direction, float maxValue)
@@ -69,5 +77,17 @@ public class UnitMover : MonoBehaviour, IService
         {
             _rigidBody.velocity = direction * maxValue;
         }
+    }
+}
+
+public static class AnimationParameters
+{
+    public static class Params
+    {
+        public static string Walk = "Walk";
+        public static string Left = "Left";
+        public static string Right = "Right";
+        public static string Back = "Back";
+        public static string Stay = "Stay";
     }
 }
