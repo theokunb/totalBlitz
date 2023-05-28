314635f8225aef653b6cdf3535b400569435c6a8
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2023-05-28 12:39:36 +0900

    read and write score

---
+++ Assets/Scripts/Model/Storage/Data.cs
@@ -0,0 +1,60 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[Serializable]
+public class Data
+{
+    public DateTime Date { get; set; }
+    public int Score { get; set; }
+}
+
+[Serializable]
+public class LeaderboardData
+{
+    [JsonProperty("TopCount")] private int _topCount;
+    [JsonProperty("Datas")] private List<Data> _datas;
+
+    public LeaderboardData() 
+    {
+        _topCount = 10;
+        _datas = new List<Data>();
+    }
+
+    public bool TryAdd(Data data)
+    {
+        var sorted = _datas.OrderByDescending(element => element.Score)
+            .Take(_topCount)
+            .ToList();
+
+        var lowerThanCurrent = sorted.Where(element => element.Score < data.Score).FirstOrDefault();
+
+        if(lowerThanCurrent != null)
+        {
+            int index = sorted.IndexOf(lowerThanCurrent);
+
+            sorted.Insert(index, data);
+
+            while(sorted.Count > _topCount)
+            {
+                sorted.RemoveAt(sorted.Count - 1);
+            }
+
+            _datas = sorted;
+            return true;
+        }
+        else
+        {
+            if(sorted.Count < _topCount)
+            {
+                _datas.Add(data);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
